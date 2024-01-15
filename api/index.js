29f939e66e29f013bd6507a5f38c3fd9d1bb40a0
Hicham Abekiri <itshichabk@gmail.com>
2024-01-15 16:33:13 +0100

    .....

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ let page;
 let fetching = false;
 
 (async()=>{
-    const browser =  await firefox.launch({executablePath: '/vercel/.cache/ms-playwright/firefox-1429/firefox/firefox'});
+    const browser =  await firefox.launch({executablePath: '/vercel/.cache/ms-playwright/firefox-1429/firefox'});
     page = await browser.newPage();
     console.log("Browser and page initialized")
     initialized = true;
